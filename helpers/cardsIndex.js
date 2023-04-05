e44671ec7866337bdaf4c17400de2ba5bf6b46ad
Gonzalo Rodriguez <gonzalo-ezequiel@hotmail.com>
2023-04-05 13:33:24 -0300

    realice el renderizado dinamico de las card del home

---
+++ helpers/cardsIndex.js
@@ -0,0 +1,33 @@
+import colita from "../img/colita_cuadril.webp";
+import bife from "../img/bife_ancho.webp";
+import chinchu from "../img/chinchu.webp";
+import provo1 from "../img/provo1.webp";
+import salchicha from "../img/salchicha.webp";
+import tomahowk from "../img/tomahowk.webp";
+
+export default cardIndex = [
+  {
+    colita,
+    h2: "Piezas enteras",
+  },
+  {
+    bife,
+    h2: "Cortes sin hueso",
+  },
+  {
+    chinchu,
+    h2: "Cortes con hueso",
+  },
+  {
+    provo1,
+    h2: "Embutidos frescos",
+  },
+  {
+    salchicha,
+    h2: "Achuras",
+  },
+  {
+    tomahowk,
+    h2: "Queso para parrilla",
+  },
+];
--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 const d = document;
+const cardIndex = [
+  {
+    img: "./img/colita_cuadril.webp",
+    h2: "Piezas enteras",
+  },
+  {
+    img: "./img/bife_ancho.webp",
+    h2: "Cortes sin hueso",
+  },
+  {
+    img: "./img/chinchu.webp",
+    h2: "Cortes con hueso",
+  },
+  {
+    img: "./img/provo1.webp",
+    h2: "Embutidos frescos",
+  },
+  {
+    img: "./img/salchicha.webp",
+    h2: "Achuras",
+  },
+  {
+    img: "./img/tomahowk.webp",
+    h2: "Queso para parrilla",
+  },
+];
 
 const navSlide = (panelBtn, panel) => {
   d.addEventListener("click", (e) => {
@@ -12,3 +38,30 @@ const navSlide = (panelBtn, panel) => {
 d.addEventListener("DOMContentLoaded", (e) => {
   navSlide(".btn-panel", ".panel");
 });
+
+function renderizarCards() {
+  const $CARD_CONTAINER = d.querySelector(".card-container");
+  cardIndex.forEach((card) => {
+    let $link = d.createElement("a");
+    let $card = d.createElement("div");
+    let $img = d.createElement("img");
+    let $cardText = d.createElement("div");
+    let $h2 = d.createElement("h2");
+
+    $link.classList.add("card");
+    $cardText.classList.add("card-text");
+    $img.src = card.img;
+    $h2.textContent = card.h2;
+    $link.href = "./pages/productos.html";
+
+    console.log($link);
+
+    $card.appendChild($img);
+    $card.appendChild($cardText);
+    $cardText.appendChild($h2);
+    $link.appendChild($card);
+    $CARD_CONTAINER.appendChild($link);
+  });
+}
+
+renderizarCards();
